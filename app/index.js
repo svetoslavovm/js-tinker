fad7876009b865e2725eaede97acb79b61a57c52
svetoslavovm <svetoslavov.martin@gmail.com>
2016-03-27 13:44:25 -0400

    adds nested components w/ props

--- app/index.js
+++ app/index.js
@@ -9,7 +9,55 @@ var HelloWorld = React.createClass({
   }
 });
 
+var HelloUser = React.createClass({
+  render: function () {
+    return (
+      <div>Hello, {this.props.name}</div>
+    )
+  }
+});
+
+var FriendsContainer = React.createClass({
+  render: function () {
+    var name = 'Andre Ward';
+    var friends = ['Bob Marley', 'Stephen King', 'Karl May'];
+    return (
+      <div>
+        <h3>Name: {name}</h3>
+        <ShowList names={friends} />
+      </div>
+    )
+  }
+})
+
+var ShowList = React.createClass({
+  render: function () {
+    var listItems = this.props.names.map(function (friend) {
+      return <li>{friend}</li>;
+    });
+
+    return (
+      <div>
+        <h3>Friends</h3>
+        <ul>
+          {listItems}
+        </ul>
+      </div>
+    )
+  }
+});
+
 ReactDOM.render(
   <HelloWorld />,
   document.getElementById('app')
+);
+
+ReactDOM.render(
+  <HelloUser name="Martin" />,
+  document.getElementsByClassName('user-greeting')[0]
+);
+
+ReactDOM.render(
+  <FriendsContainer />,
+  document.getElementsByClassName('friend-list')[0]
 );
